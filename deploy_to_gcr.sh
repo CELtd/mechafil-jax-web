a52e150bb06c57b2bc66696147e7507c3917537a
Kiran Karra <kiran.karra@gmail.com>
2025-07-18 11:09:20 -0700

    adding deploy script

---
+++ deploy_to_gcr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gcloud builds submit --tag gcr.io/cel-streamlit/mechafil-jax-web
+
+gcloud run deploy mechafil-jax-web \
+  --image gcr.io/cel-streamlit/mechafil-jax-web \
+  --platform managed \
+  --region us-central1 \
+  --allow-unauthenticated \
+  --min-instances=0 \
+  --port=8501
